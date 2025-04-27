862ef45485243adce7655c78d3ed9c330f05108a
Roberto <san20086@byui.edu>
2025-04-26 21:51:55 -0600

    Made hello world program.

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+#Welcome to my Hello World program. This program intents to show my knowledge on basic python. 
+
+name = input("What is your name?")
+
+where_you_are_from = input("Where are you from?")
+
+hello_world = "Hello World!!"
+
+print(f'{name}, from {where_you_are_from}. Welcome to my first "{hello_world}"')
+
